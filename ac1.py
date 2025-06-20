f678555c1906b6d14cbee9464544f2abae42c8d6
Nihitha2013 <sowjanyay2020@gmail.com>
2025-06-20 18:53:20 +0530

    Add files via upload

---
+++ ac1.py
@@ -0,0 +1,6 @@
+from tkinter import *
+window=Tk()
+window.title("widget")
+window.geometry("400x400")
+
+window.mainloop()
---
+++ ac3.py
@@ -0,0 +1,31 @@
+from tkinter import *
+window=Tk()
+window.title("widget")
+window.geometry("400x400")
+l1=Label(text="Welcome",fg="white",bg="blue",height=1,width=300)
+from PIL import Image, ImageTk
+lab_img=Image.open("images.jpg")
+img1=ImageTk.PhotoImage(lab_img)
+label=Label(window, image=img1,height=200,width=300)
+
+
+l2=Label(text="Welcome",fg="black",bg="light green")
+e1=Entry()
+
+def display():
+    name=e1.get()
+    global msg
+    msg="Hello"+name+"Nice to meet you"
+    tb.insert(END,msg)
+
+tb=Text(height=3)
+b=Button(text="click",command=display,height=1)
+
+l1.pack()
+label.pack()
+l2.pack()
+e1.pack()
+b.pack()
+tb.pack()
+
+window.mainloop()
